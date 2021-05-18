e1e6c275e36dcdaac44e4a61fd443c7fc65eb583
yesid gutierrez <ygutierrezd@outlook.com>
2021-05-17 21:15:57 -0500

    configuracion inicial

---
+++ Ejecutar/Program.cs
@@ -0,0 +1,15 @@
+﻿using Elasticsearch_NET;
+using System;
+
+namespace Ejecutar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ESLowLevelClient es = new ESLowLevelClient();
+            es.con();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Elasticsearch_NET/ESLowLevelClient.cs
@@ -0,0 +1,36 @@
+﻿using Elasticsearch.Net;
+using System;
+
+namespace Elasticsearch_NET
+{
+    public class ESLowLevelClient
+    {
+
+        public void con(){
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"))
+                    .RequestTimeout(TimeSpan.FromMinutes(2));
+            var connection = new ElasticLowLevelClient(settings);
+
+            var searchResponse = connection.Search<StringResponse>("test", PostData.Serializable(new
+            {
+                from = 0,
+                size = 10,
+                query = new
+                {
+                    match = new
+                    {
+                        nombre = new
+                        {
+                            query = "Yesid2"
+                        }
+                    }
+                }
+            }));
+
+            var successful = searchResponse.Success;
+            var responseJson = searchResponse.Body;
+            Console.WriteLine(responseJson);
+        }       
+
+    }
+}
